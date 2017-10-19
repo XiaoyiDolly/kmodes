3a959a0eb48fb1fa9a0102dfe4e2c0fe6f9df751
dxy <dolly.d777@gmail.com>
2017-10-19 15:12:41 -0700

    compare category

--- test.py
+++ test.py
@@ -9,38 +9,110 @@ from kmodes.kmodes import KModes
 # reload(sys)
 # sys.setdefaultencoding('utf8')
 
-ids = open('data_names/ids.csv', 'r', encoding='utf8')
+
+def init(cat='n&c'):
+    if cat =='n&c':
+        ids = open('data_category/ids.csv', 'r', encoding='utf8')
+        lines = ids.readlines()
+        id_name = {}
+        id_cat = {}
+        for l in lines:
+            l = l.split(';')
+
+            if len(l) ==3:
+                id_cat[l[0]] = l[2].split('>')
+                id_name[l[0]] = l[1].lower()
+            else:
+                id_cat[l[0]] = l[1].split('>')
+                id_name[l[0]] = l[1].split('>')[-1].lower()
+    elif cat == 'n':
+        ids = open('data_names/ids.csv', 'r', encoding='utf8')
+        lines = ids.readlines()
+        id_name = {}
+        for l in lines:
+            l = l.split(' ',1)
+            id_name[l[0]] = l[1].lower()
+    return id_name, id_cat
+
+ids = open('data_category/ids.csv', 'r', encoding='utf8')
 lines = ids.readlines()
 id_name = {}
+id_cat = {}
 for l in lines:
-    l = l.split(' ',1)
-    id_name[l[0]] = l[1].lower()
-    # print(l[0])
+    l = l.strip().split(';')
+    # print(l)
+    if len(l) ==3:
 
-def semantic_dismilarity(a, b):
-    if '' in a or '' in b:
+        id_cat[l[0]] = l[2].split('>')
+        id_name[l[0]] = l[1].lower()
+    else:
+        # print('2222222', l)
+        id_cat[l[0]] = l[1].split('>')
+        id_name[l[0]] = l[1].split('>')[-1].lower()
+    if l[0] ==77:
+        print('*********',l)
+
+print (id_cat['0'])
+print (id_cat['82'])
+print (id_cat['6543'])
+print (id_cat['143'])
+
+def semantic_disimilarity(a, b):
+    print(a, b)
+    if not a or not b:
         # print(1)
         return 1
     namea = ''
     nameb = ''
-    if 'g' not in a and 'g' not in b:
-        lista = list(map(lambda x: id_name[x[1:]].strip() if x else '',a.split(' ')))
-        namea = ' '.join(lista)
-        listb = list(map(lambda x: id_name[x[1:]].strip() if x else '',b.split(' ')))
-        nameb = ' '.join(listb)
-    else:
-        lista = list(map(lambda x: str.replace(id_name[x[1:]].strip(), '>',' ') if x else '', a.split(' ')))
-        namea = ' '.join(lista)
-        listb = list(map(lambda x: str.replace(id_name[x[1:]].strip(), '>',' ') if x else '', b.split(' ')))
-        nameb = ' '.join(listb)
+    # if 'g' not in a and 'g' not in b:
+    lista = list(map(lambda x: id_name[x[1:]].strip() if x else '',a.split(' ')))
+    namea = ' '.join(lista)
+    listb = list(map(lambda x: id_name[x[1:]].strip() if x else '',b.split(' ')))
+    nameb = ' '.join(listb)
 
 
     if len(lista) <=len(listb):
         sim = sum(map(lambda x: x in nameb, namea.split(' '))) / len(namea.split(' '))
     else:
         sim = sum(map(lambda x: x in namea, nameb.split(' '))) / len(nameb.split(' '))
-    # print(lista, listb, sim)
+    print('sem sim: ', sim)
     return 1-sim
+
+def jaccard(a, b):
+    a = set(a)
+    b = set(b)
+    return len(a&b) / len(a|b)
+
+def class_disimilarity(a, b):
+    # print(',,,',a.split(' '))
+    # print(',,,',b.split(' '))
+    if not a or not b:
+        # print(1)
+        return 1
+    classa = []
+    for x in a.split(' '):
+        classa.append(id_cat[x[1:]])
+    # map(lambda x: classa.append(id_cat[x[1:]]), a.split(' '))
+    classb = []
+    for x in b.split(' '):
+        classb.append(id_cat[x[1:]])
+    # map(lambda x: classb.append(id_cat[x[1:]]), b.split(' '))
+    print(classa)
+    print(classb)
+    if len(classa) <=len(classb):
+        maxsim = 0
+        for c in classa:
+            sim = max(map(lambda x: jaccard(c, x), classb))
+            if sim > maxsim:
+                maxsim = sim
+    else:
+        maxsim = 0
+        for c in classb:
+            sim = max(map(lambda x: jaccard(c, x), classa))
+            if sim > maxsim:
+                maxsim = sim
+    print('cat sim: ', maxsim)
+    return 1-maxsim
 
 def multimatch_dissim(a, b, dec_map):
     nattrs = a.shape[1]
@@ -63,21 +135,31 @@ def multimatch_dissim(a, b, dec_map):
         for c, vc in enumerate(vr):
             valuea = vc.strip()
             valueb = strb[c].strip()
-            if not any(i in valuea.split(' ')  for i in valueb.split(' ')) or not valuea or not valueb:
-#                print('column a--------------->>>>', valuea)
-#                print('column b--------------->>>>', valueb)
-                # print(semantic_dismilarity(valuea, valueb))
-                sum+= semantic_dismilarity(valuea, valueb)
+            # if valuea
+            # if not any(i in valuea.split(' ')  for i in valueb.split(' ')) or not valuea or not valueb:
+            print('column a--------------->>>>', valuea)
+            print('column b--------------->>>>', valueb)
+            # print(semantic_disimilarity(valuea, valueb))
+            # print(class_disimilarity(valuea, valueb))
+            if 'd' in valuea or 'd' in valueb:
+                print('include d')
+                sum+= semantic_disimilarity(valuea, valueb)
+            else:
+                print('not include d')
+                sum +=0.3 * semantic_disimilarity(valuea, valueb) + 0.7*class_disimilarity(valuea, valueb)
+
         dis[r] = sum/4
     print(dis)
     return dis
 
-
+# global id_name
+# global id_cat
+# id_name, id_cat = init('n&c')
 # reproduce results on small data set
-x = np.genfromtxt('data_names/dataset_extract.csv', dtype=str, delimiter=',')[:, 0:]
-y = np.genfromtxt('data_names/dataset_extract.csv', dtype=str, delimiter=',', usecols=(0 ))
-# x = np.genfromtxt('data/dataset_extract.csv', dtype=str, delimiter=',')[:, 1:]  # test.csv
-# y = np.genfromtxt('data/dataset_extract.csv', dtype=str, delimiter=',', usecols=(0 ))
+# x = np.genfromtxt('data_names/dataset_extract.csv', dtype=str, delimiter=',')[:, 0:]
+# y = np.genfromtxt('data_names/dataset_extract.csv', dtype=str, delimiter=',', usecols=(0 ))
+x = np.genfromtxt('data_category/dataset_extract.csv', dtype=str, delimiter=',')[:, 0:]  # test.csv
+y = np.genfromtxt('data_category/dataset_extract.csv', dtype=str, delimiter=',', usecols=(0 ))
 print(y)
 print(x.shape)
 print(y.shape)
